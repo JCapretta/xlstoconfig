4e8d1884f8580410e8f742903ec20635bb1d5c3f
John Capretta <John.Capretta@gmail.com>
2017-10-10 13:54:21 +1100

    added ability to derive row attributes/columns from spreadsheet

--- test_xlstoconfig.py
+++ test_xlstoconfig.py
@@ -1,12 +1,54 @@
 #!/usr/bin/python
 import unittest
-from xlstoconfig import jinjaSkeleton
+from xlstoconfig import *
+import xlstoconfig
+import os
 
-class Test_jinjaSkeleton(unittest.TestCase):
+argparse_help = '''
+usage: test_xlstoconfig.py [-h] [-f FILE] [-t TEMPLATE] [-j]
+
+John's config generator
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FILE, --file FILE  yaml file
+  -t TEMPLATE, --template TEMPLATE
+                        Jinja2 template file
+  -j                    Generate a skeleton for building a jinja2 template
+'''
+
+
+class Test_args(unittest.TestCase):
+
     def setUp(self):
         pass
-    def test_parent_name_type(self):
-        self.assertEqual('blah',jinjaSkeleton('blah','blah'))
-
+    def test_no_args(self):
+        #result = get_args()
+        with self.assertRaises(SystemExit) as cm:
+            get_args([])
+        print cm.exception
+        self.assertEqual(cm.exception.code,1)
+    def test_j_flag(self):
+        args=get_args(['file','-j'])
+        self.assertEqual(args.create_skeleton,True)
+    def test_template_arg(self):
+        args=get_args(['file'])
+        self.assertEqual(args.file,'file')
+        
+class Test_Runner(unittest.TestCase):
+    def Setup(self):
+        pass
+    def test_load_yaml(self):
+        with self.assertRaises(SystemExit) as cm:
+            runner=Runner(['test'])
+            runner.load_yaml()
+        print cm.exception
+        self.assertEqual(cm.exception.code,4)
+        
+        
+       
+		
+		
+		
 if __name__ == '__main__':
     unittest.main()
--- xlstoconfig.py
+++ xlstoconfig.py
@@ -4,10 +4,17 @@ from openpyxl import load_workbook
 import argparse
 from jinja2 import Environment, FileSystemLoader
 import yaml
+import sys
 
-class PanoramaConfig:
+class Config:
     def __init__(self, yaml, wb=None):
-        self.wb = wb
+        if 'workbook' in yaml:
+            if not yaml['workbook']=='PARENT_WORKBOOK':
+                self.wb = load_workbook(filename = yaml['workbook'], data_only=True)
+            else:
+                self.wb = wb
+        else:
+            self.wb = wb
         if 'children' in yaml:
         #create dictionary attributes of this object
             self.make_dict_attributes(yaml['children'])
@@ -18,16 +25,21 @@ class PanoramaConfig:
         
     def read_attributes(self,yaml,attr):
     #read the yaml to get the pointers to a table within a spreadsheet. each row is a new child object within a dictionary attribute of a parent object
-        self.wb,ws,columns = self.get_locations(yaml,self.wb)
+        wb,ws,columns = self.get_locations(yaml,self.wb)
+        autocolumns = self.auto_column(yaml,wb) 
+        
+        for key in autocolumns.keys():
+            if key not in columns.keys():
+                columns[key]=autocolumns[key]
         if ws:
             range_string = yaml['range'] 
             for row in ws[range_string]:
                 name = row[columns['name']].value
                 if name not in getattr(self,attr):
-                    getattr(self,attr).update({name:PanoramaConfig(yaml,self.wb)})
+                    getattr(self,attr).update({name:Config(yaml,wb)})
                 self.make_class_attributes(getattr(self,attr)[name],row,columns)
         else:
-            setattr(self,attr,PanoramaConfig(yaml,self.wb))
+            setattr(self,attr,Config(yaml,wb))
         
     def set_attributes(self,attrs):
     #create scalar attributes of this object
@@ -42,8 +54,6 @@ class PanoramaConfig:
             else:
                 setattr(self,key,[attrs[key]])
 
-            
-    
     def make_dict_attributes(self,children):
     #create atrributes of this object that are to be dictionaries
         for key in children.keys():
@@ -53,24 +63,23 @@ class PanoramaConfig:
                 setattr(self,key,getattr(self,key+"_dict").values())
             else:
                 setattr(self,key,getattr(self,key+"_dict"))
+                
     def make_class_attributes(self,obj,row,columns):
         rowvals={}
         rowlistvals={}
         if 'lists' in columns:
             for list in columns['lists']:
-                rowlistvals[list]=row[int(columns['lists'][list])].value
+                rowlistvals[list]=row[int(columns[' '][list])].value
             obj.append_attributes(rowlistvals)
         for column in columns.keys():
             if not column=='lists':
                 if columns[column] or columns[column]==0:#test if 0. zero is not null
                     rowvals[column]=row[int(columns[column])].value        
                     obj.set_attributes(rowvals)
-
-        
+  
     def get_locations(self,locations,wb=None):
         #load workbook
-        columns=None
-        #wb=None
+        columns={}
         ws=None
         if 'workbook' in locations:
             if not locations['workbook']=='PARENT_WORKBOOK':
@@ -78,12 +87,28 @@ class PanoramaConfig:
             ws = wb[locations['worksheet']]
         if 'columns' in locations:
             columns=locations['columns']
-            
         return wb,ws,columns
+    
+    def auto_column(self,yaml,wb):
+        #automatically generate the 'column' dictionary
+        ws = wb[yaml['worksheet']]
+        range_string = yaml['range']
+        cols={}
+        #get header row
+        first_col = range_string.split(":")[0][0]
+        last_col = range_string.split(":")[1][0]
+        top_row = int(range_string.split(":")[0][1])
+        top_row = 1 if top_row == 1 else top_row-1 #if top row of range is already at the top don't decrement. otherwise decrement.
+        header_range = first_col+str(top_row)+':'+last_col+str(top_row)
+        #print header_range
+        header = ws[header_range]
+        #print "header len "+str(len(header[0]))
+        for i in range(len(header[0])):
+            cols[header[0][i].value]=i 
+        cols['name']=ord(first_col.upper())-65
+        return cols
         
-
-                
-                
+                 
 def jinjaSkeleton(parent_name,yaml):
     #create a skeleton for your jinja template. good to use as a starting point if you don't have a jinja template already
     if 'children' in yaml:
@@ -97,34 +122,69 @@ def jinjaSkeleton(parent_name,yaml):
     else:
         return ' '
 
-                
-def main():
-    
-    #arguments
-    parser = argparse.ArgumentParser(description="John's config generator")
-    parser.add_argument('-f', '--file',help='config workbook')
-    parser.add_argument('-t', '--template')
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+    #override error method to print help and error message
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+		
+def get_args(args=sys.argv[1:]):
+    parser = MyParser(description="John's config generator")
+    parser.add_argument('file',help='yaml file')
+    parser.add_argument('-t', '--template', help='Jinja2 template file')
     parser.add_argument('-j', dest='create_skeleton', help='Generate a skeleton for building a jinja2 template', action='store_true')
-    args = parser.parse_args()
-    
-    #yaml
-    
-    with open(args.file, 'r') as file_descriptor:
-        locations = yaml.load(file_descriptor)
-     
-    # print the jinja skeleton. 
-    if args.create_skeleton:
-        print jinjaSkeleton('config',locations)
+    if len(args)==0:
+    #print help if no arguments
+        parser.print_help()
+        sys.exit(1)
+    parsed_args = parser.parse_args(args)
+    return parsed_args 
+        
+class Runner:
+    def __init__(self, args=sys.argv[1:]):
+        self.p_args=get_args(args)
+        self.load_yaml()
+        self.root='config'
+        if self.p_args.create_skeleton:
+            print self.jinjaSkeleton(self.root,self.locations)
+        self.config=Config(self.locations)
+        if self.p_args.template:
+            print self.render_template()
+        
+    def load_yaml(self):
+        if self.p_args.file:
+            try:
+                with open(self.p_args.file, 'r') as file_descriptor:
+                    self.locations = yaml.load(file_descriptor)
+            except Exception as ex:
+                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                message = template.format(type(ex).__name__, ex.args)
+                print message
+                sys.exit(4) 
+                
+    def jinjaSkeleton(self,parent_name,yaml):
+        #create a skeleton for your jinja template. good to use as a starting point if you don't have a jinja template already
+        if 'children' in yaml:
+            jinja_text=''
+            for key in yaml['children'].keys():
+                if 'workbook' in yaml['children'][key]:
+                    jinja_text += '{% for ' + key + ' in ' + parent_name + '.' + key + ' %}'+ '\n' + jinjaSkeleton(key,yaml['children'][key]) + '\n{% endfor %}\n'    
+                else:
+                    jinja_text += '{% set ' + key + ' = ' + parent_name + '.' + key + ' %}'+ '\n' + jinjaSkeleton(key,yaml['children'][key])
+            return jinja_text
+        else:
+            return ' '            
 
-    #initialize switch config
-    config = PanoramaConfig(locations)
-    
-    # render template and print
-    if args.template:
+    def render_template(self):
         env = Environment(loader=FileSystemLoader('./'))
-        template = env.get_template(args.template)
-        print template.render(config = config)
-    
+        template = env.get_template(self.p_args.template)
+        return template.render(config = self.config)
+        
+	
+def main():
+
+    my_runner=Runner()
 
 if __name__ == '__main__':
   main()
